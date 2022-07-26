2e426762451abb6f77330f91f20d541566dbc85e
Samara Marques <103200488+samrqs@users.noreply.github.com>
2022-07-26 15:24:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for your program.
+print("Bem vindo ao gerador de nome de banda!")
+#2. Ask the user for the city that they grew up in.
+cidade = input("Em qual cidade você nasceu?\n")
+#3. Ask the user for the name of a pet.
+pet = input("Qual o nome do seu pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("O nome da sua banda pode ser: " + cidade + " " + pet)
+#Projeto simples em Python 
